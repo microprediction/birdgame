5e02509b6ea0596d400e75d786ae04e0641619af
microprediction <peter.cotton@microprediction.com>
2025-02-11 09:03:05 -0500

    tests

--- birdgame/datasources/remotetestdata.py
+++ birdgame/datasources/remotetestdata.py
@@ -1,10 +1,21 @@
 import pandas as pd
 
 
-def remote_test_data()->pd.DataFrame:
-    return pd.read_csv('')
+def remote_test_data() -> pd.DataFrame:
+    return pd.read_csv(
+        'https://raw.githubusercontent.com/microprediction/birdgame/refs/heads/main/data/bird_feed_data.csv')
 
 
-if __name__=='__main__':
+def remote_test_data_generator():
+    """
+      Generate the remote test data yielding one record (dict) at a time
+    :return:
+    """
+    df = remote_test_data()
+    for _, row in df.iterrows():
+        yield row.to_dict()
+
+
+if __name__ == '__main__':
     df = remote_test_data()
     print(df[:3])
---
+++ tests/datasources/test_remote_test_data.py
@@ -0,0 +1,20 @@
+from birdgame.datasources.remotetestdata import remote_test_data, remote_test_data_generator
+import pandas as pd
+import pytest 
+
+
+def test_remote_test_data():
+    df = remote_test_data()
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) > 0
+
+
+def test_remote_test_data_generator():
+    generator = remote_test_data_generator()
+    first_record = next(generator)
+    assert isinstance(first_record, dict)
+    assert len(first_record) > 0
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
