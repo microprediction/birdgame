e3b96bea6e72d53f433b951d0fc75737e8aeb052
microprediction <peter.cotton@microprediction.com>
2025-03-18 11:43:05 -0400

    added ability to run example ets

--- birdgame/examples/derived/autoets.py
+++ birdgame/examples/derived/autoets.py
@@ -148,3 +148,12 @@ if using_sktime:
 
 else:
     AutoETSsktimeTracker = None
+
+
+
+if __name__ == '__main__':
+    tracker = AutoETSsktimeTracker()
+    tracker.test_run(
+        live=False, # Set to True to use live streaming data; set to False to use data from a CSV file
+        step_print=10000 # Print the score and progress every 1000 steps
+    )
