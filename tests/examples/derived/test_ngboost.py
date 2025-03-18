48145b90cd516bf30157bf359677f3182da9405b
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2025-03-18 11:49:15 -0400

    Added ngboost example (#5)
    * Added ngboost example
    
    * added ability to run example ets

---
+++ tests/examples/derived/test_ngboost.py
@@ -0,0 +1,13 @@
+
+from birdgame.examples.derived.ngboosttracker import NGBoostTracker
+
+
+
+def test_ngboost_test_run():
+    if NGBoostTracker is not None:
+        tracker = NGBoostTracker()
+        tracker.test_run(
+            max_rows=1000,
+            live=False, # Set to True to use live streaming data; set to False to use data from a CSV file
+            step_print=10000 # Print the score and progress every 1000 steps
+        )
