058ae6d9db2c5a3017ff2dafcad8391937070b11
Caceresenzo <caceresenzo1502@gmail.com>
2025-06-24 17:04:13 +0200

    fix(redis): load certificate using official means

--- birdgame/config/getredisconfig.py
+++ birdgame/config/getredisconfig.py
@@ -1,18 +1,19 @@
-import os
-
-CERTIFICATES_DIRECTORY = os.path.dirname(__file__)
+import importlib.resources
 
 
 def get_redis_config():
-    return {'username': 'public',
-            'password': 'public',
-            'host': '185.212.80.70',
-            'port': 6381,
-            'ssl_ca_certs': os.path.join(CERTIFICATES_DIRECTORY, 'certificates/ca.crt'),
-            'ssl': True,
-            'decode_responses': True,
-            "ssl_cert_reqs": "none"
-            }
+    ca_crt = importlib.resources.read_text(__package__ + ".certificates", 'ca.crt')
+
+    return {
+        'username': 'public',
+        'password': 'public',
+        'host': '185.212.80.70',
+        'port': 6381,
+        'ssl_ca_data': ca_crt,
+        'ssl': True,
+        'decode_responses': True,
+        "ssl_cert_reqs": "none"
+    }
 
 
 if __name__ == '__main__':
