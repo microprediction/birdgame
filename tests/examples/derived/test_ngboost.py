8fda76e3e1e7d9d992130594c7416b7c04d7b53b
abde-crunchdao <abdennour.boutrig@crunchdao.com>
2025-10-17 11:16:51 +0200

    New static data sources (#20)

--- tests/examples/derived/test_ngboost.py
+++ tests/examples/derived/test_ngboost.py
@@ -9,5 +9,5 @@ def test_ngboost_test_run():
         tracker.test_run(
             max_rows=1000,
             live=False, # Set to True to use live streaming data; set to False to use data from a CSV file
-            step_print=10000 # Print the score and progress every 1000 steps
+            step_print=1000 # Print the score and progress every 1000 steps
         )
