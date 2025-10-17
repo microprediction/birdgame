a14861e8e97501ba6562d2363b0ee3418363c8e8
boutrig abdennour <abdennour.boutrig@crunchdao.com>
2025-10-17 10:24:57 +0200

    New static data sources

--- birdgame/examples/derived/ewmatracker.py
+++ birdgame/examples/derived/ewmatracker.py
@@ -118,5 +118,5 @@ if __name__ == '__main__':
     tracker = EMWAVarTracker()
     tracker.test_run(
         live=False, # Set to True to use live streaming data; set to False to use data from a CSV file
-        step_print=10000 # Print the score and progress every 1000 steps
+        step_print=1000 # Print the score and progress every 1000 steps
     )
