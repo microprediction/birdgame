e906e55d83ed74ee527d90e44afdcc20ae97ccd3
Tarandro <alexis.gassmann2@orange.fr>
2025-11-06 17:06:02 +0100

    add warmup phase comment

--- birdgame/constants.py
+++ birdgame/constants.py
@@ -12,8 +12,9 @@ GAME_PARAMS = {
     "initial_wealth": 1000,        # starting wealth per player
 
     # FYI: 1000 steps/ticks ~ 1min
-    "alpha_short": 1 / (20 * 1000 / 60),  # (20 seconds) Smoothing factor for exponentially weighted moving average of short-term log-likelihood 
-    "alpha_long": 1 / (20 * 1000),        # (20 minutes) Smoothing factor for exponentially weighted moving average of long-term log-likelihood
+    "alpha_short": 1 / (20 * 1000 / 60),  # (short=20 seconds) Smoothing factor for exponentially weighted moving average of short-term log-likelihood 
+    "alpha_long": 1 / (20 * 1000),        # (long=20 minutes) Smoothing factor for exponentially weighted moving average of long-term log-likelihood
     "w_short": 0.5,                       # Weighting factor to blend short-term log-likelihood vs long-term log-likelihood
     "ewma_weight": 1.0,                   # Weighting factor to blend EWMA vs instantaneous likelihood for wealth redistribution
+    # When submitted to the platform, the model will be in a warmup phase for at least long=20 minutes.
 }
